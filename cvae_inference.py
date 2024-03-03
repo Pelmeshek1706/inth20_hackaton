1df57abf1c6f0368399a1e49632ca37754015b24
Pelmeshek1706 <basenko.nikita.r@gmail.com>
2024-03-03 09:23:50 +0200

    add new

--- cvae_inference.py
+++ cvae_inference.py
@@ -3,19 +3,30 @@ from cvae import ConditionalVAE
 from torchvision.utils import save_image
 from argparse import ArgumentParser
 import os
+from huggingface_hub import hf_hub_download
 
 
 def inference():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    repo_id = "poopaandloopa/int20h_cvae"
+    filename = "cvae_epoch_15.pt"
+    destination_dir = "weights/"
+
     parser = ArgumentParser()
     parser.add_argument('--cluster', type=int)
-    parser.add_argument('--weights', type=str, required=False, default='weights/cvae_epoch_8.pt')
+    parser.add_argument('--weights', type=str, required=False, default=None)
     parser.add_argument('--num_classes', type=int, required=False, default=5)
     parser.add_argument('--image_size', type=int, required=False, default=224)
     args = parser.parse_args()
 
-    model_weights = torch.load(args.weights)['model']
+    weights = args.weights
+
+    if not weights:
+        hf_hub_download(repo_id=repo_id, filename=filename, local_dir=destination_dir, local_dir_use_symlinks=False)
+        weights = destination_dir + filename
+
+    model_weights = torch.load(weights)['model']
 
     model = ConditionalVAE(in_channels=3,
                            num_classes=args.num_classes,
@@ -33,4 +44,3 @@ def inference():
 
 if __name__ == '__main__':
     inference()
-
