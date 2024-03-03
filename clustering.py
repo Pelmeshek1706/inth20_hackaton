0420f6d6b8939e94db0c9c171b349b9b6fc67e4a
Pelmeshek1706 <basenko.nikita.r@gmail.com>
2024-03-03 03:29:34 +0200

    add docstring

--- clustering.py
+++ clustering.py
@@ -4,7 +4,17 @@ from sklearn.cluster import KMeans
 from argparse import ArgumentParser
 
 
-def _load_embeddings(embeddings_path):
+def _load_embeddings(embeddings_path: str): 
+    """Loads image embeddings from a CSV file.
+
+        Args:
+            embeddings_path: Path to the CSV file containing image embeddings.
+
+        Returns:
+            A tuple containing:
+                - filenames: A list of image filenames corresponding to the embeddings.
+                - embeddings: A numpy array of embeddings, where each row represents an image.
+    """
     embeddings = []
     filenames = []
     with open(embeddings_path, 'r') as csvfile:
@@ -20,14 +30,33 @@ def _load_embeddings(embeddings_path):
     return filenames, embeddings
 
 
-def _kmeans_clustering(num_clusters, embeddings):
+def _kmeans_clustering(num_clusters: int, embeddings: np.ndarray):
+    """
+    Performs K-means clustering on the given embeddings.
+
+    Args:
+        num_clusters: The desired number of clusters.
+        embeddings: A numpy array of embeddings, where each row represents an image.
+
+    Returns:
+        A list of cluster labels for each embedding.
+    """
+    
     kmeans = KMeans(n_clusters=num_clusters, random_state=0)
     kmeans.fit(embeddings)
     cluster_labels = kmeans.labels_.tolist()
     return cluster_labels
 
 
-def _generate_clustering_results(filenames, cluster_labels, output_csv):
+def _generate_clustering_results(filenames: list[str], cluster_labels:list[int], output_csv: str):
+    """Generates a CSV file containing image filenames and their assigned cluster labels.
+
+        Args:
+            filenames: A list of image filenames.
+            cluster_labels: A list of cluster labels corresponding to the filenames.
+            output_csv: Path to the output CSV file.
+    """
+    
     with open(output_csv, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['image_name', 'class'])
